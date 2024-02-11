c3ba236a411274defdff51c04d2bca2d52a258b2
Фариза <fariza@MacBook-Air-Fariza.local>
2024-02-12 01:10:29 +0600

    f

---
+++ TSIS-4/generators/1_gen.py
@@ -0,0 +1,9 @@
+#  Create a generator that generates the squares of numbers up to some number N.
+def squares(N):
+    for i in range(N):
+        yield i ** 2
+# yield аналогично ключевому слову return, возвращает значение и "замораживает" состояние функции, чтобы она могла быть возобновлена с последней точки остановки.
+# чисел до 10
+N = 10
+for square in squares(N):
+    print(square)
