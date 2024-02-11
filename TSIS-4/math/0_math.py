c2ce2e985cfbedd2784c5bb67efea4760228f5d8
Фариза <fariza@MacBook-Air-Fariza.local>
2024-02-12 00:33:06 +0600

    f

---
+++ TSIS-4/math/0_math.py
@@ -0,0 +1,28 @@
+# Built-in Math Functions
+x = min(15 ,12 , 2)
+y = max(12, 3 , 5)
+print(x , y)
+
+#  absolute (positive) value
+x = abs(-1343.4)
+print(x)
+
+# value of x to the power of y
+x = pow(2 ,5)
+print(x)
+
+# The Math Module
+import math 
+
+x = math.sqrt(49)
+print(x)
+
+import math 
+x = math.ceil(1.2) # number upwards
+y = math.floor(1.9) # number downwards
+print(x)
+print(y)
+
+import math
+x = math.pi
+print(x)
