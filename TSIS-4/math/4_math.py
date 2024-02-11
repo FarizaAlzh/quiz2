03700d0e7263fa7b6a82d445bea55107b8753579
Фариза <fariza@MacBook-Air-Fariza.local>
2024-02-12 00:53:23 +0600

    f

---
+++ TSIS-4/math/4_math.py
@@ -0,0 +1,8 @@
+# Write a Python program to calculate the area of a parallelogram.
+import math 
+Length = 5
+Height_parallelogram = 6
+area_parallelogram = Length * Height_parallelogram
+print(float(area_parallelogram))
+
+#  Output: 30.0
