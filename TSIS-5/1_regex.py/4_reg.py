d7a4c157112aa787cf8276a406463fa5bc78f912
Фариза <fariza@MacBook-Air-Fariza.local>
2024-02-26 00:45:10 +0600

    f

--- TSIS-5/1_regex.py/4_reg.py
+++ TSIS-5/1_regex.py/4_reg.py
@@ -1,4 +1,5 @@
+# Write a Python program to find the sequences of one upper case letter followed by lower case letters.
 import re
-txt = input()
-x = re.findall(r'\b[a-z]+_[a-z]+',txt)
+txt = "Sakffof ffkjdf Skof"
+x = re.findall( r"\b[A-Z][a-z]*\b"  , txt)
 print(x)
