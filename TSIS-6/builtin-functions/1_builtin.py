65527364ed9a7d5038ad79320edac807caa6083f
Фариза <fariza@MacBook-Air-Fariza.local>
2024-03-03 20:32:40 +0500

    f

---
+++ TSIS-6/builtin-functions/1_builtin.py
@@ -0,0 +1,5 @@
+# Write a Python program with builtin function to multiply all the numbers in a list
+import math 
+num = [1 , 2 , 3, 4 , 5]
+result = math.prod(num)
+print(result)
---
+++ TSIS-6/builtin-functions/3_ builtin.py
@@ -0,0 +1,1 @@
+# Write a Python program with builtin function that checks whether a passed string is palindrome or not.
