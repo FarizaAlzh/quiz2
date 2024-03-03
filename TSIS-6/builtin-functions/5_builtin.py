a59f2a6e7ea8e0064ba7be779e79fd574e685bb0
Фариза <fariza@MacBook-Air-Fariza.local>
2024-03-04 00:27:27 +0500

    f

---
+++ TSIS-6/builtin-functions/5_builtin.py
@@ -0,0 +1,5 @@
+# Write a Python program with builtin function that returns True if all elements of the tuple are true
+tuple1 = (True , False , True)
+tuple2 = (True , True  , True)
+print(all(tuple1))
+print(all(tuple2))
