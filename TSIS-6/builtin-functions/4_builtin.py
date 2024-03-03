d5733f074680d9f89adf14ff1714560cad5c0a7c
Фариза <fariza@MacBook-Air-Fariza.local>
2024-03-04 00:23:56 +0500

    f

---
+++ TSIS-6/builtin-functions/4_builtin.py
@@ -0,0 +1,12 @@
+# Write a Python program that invoke square root function after specific milliseconds.
+import time  # time.sleep()чтобы создать паузу в выполнении 
+import math  # math.sqrt()
+
+Square_root = 25100
+miliseconds = 2123
+time.sleep(miliseconds / 1000)
+print("Square root of 25100 after 2123 miliseconds is ", math.sqrt(Square_root))
+
+
+
+
