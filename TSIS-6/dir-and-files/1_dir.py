a495b3bd6c2286539ad789c15b7e2ad2570b436c
Фариза <fariza@MacBook-Air-Fariza.local>
2024-03-04 09:36:26 +0500

    f

--- TSIS-6/dir-and-files/1_dir.py
+++ TSIS-6/dir-and-files/1_dir.py
@@ -1,3 +1,4 @@
+# EX 1
 # Write a Python program to list only directories, files and all directories, files in a specified path.
 import os # для взаимодействия с операционной системой
 # перечислить только каталоги, файлы и все каталоги, файлы в указанном пути.
@@ -31,4 +32,48 @@ print("\nСписок всех каталогов и файлов:")
 list_all_dir_file(path)
 
 # TSIS-6
+
+
+# EX 2
+if os.path.exists(path): 
+    print("The path exists")
+else:
+    print("The path does not exist")
+
+if os.access(path, os.R_OK): #доступно ли чтение
+    print("You can read the path")
+else:
+    print("Unable to read path")
+
+if os.access(path, os.W_OK):
+    print("Can be write")
+else:
+    print("Unable to write")
+
+if os.access(path, os.X_OK):
+    print("You can follow the path")
+else:
+    print("Unable to execute path")
+
+
+# EX 3
+if os.path.exists(path):
+    dir, file = os.path.split(path)
+    print(f"File name: {file}")
+    print(f"Part of the catalog: {dir}")
+else:
+    print("The specified path does not exist.")
+
+# EX 4 
+with open(r"TSIS-6/exaples/demofile.txt", 'r') as file:
+    lines = len(file.readlines())
+    print('Number of lines:', lines)
+
+# EX 5 
+list = ['Fariza', 'Adal', 'Sultan']
+
+with open('TSIS-6/exaples/demofile.txt' , 'w') as file:
+    for i in list:
+        file.write(i + '\n')
+file.close()
 
